d5c13afb7e9fd9a358c1f30366de78637a5b3938
AldonIsenberg11 <aldon.isenberg@flosports.tv>
2022-01-21 15:56:41 -0600

    throw not found error

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -34,7 +34,13 @@ export class TasksService {
   }
 
   public getTaskById(id: string) {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return task;
   }
 
   public createTask(createTaskDto: CreateTaskDto): Task {
