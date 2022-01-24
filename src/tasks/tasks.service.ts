a4c9b99e978612e2775fc065c62a02e0ff6faaa2
AldonIsenberg11 <aldon.isenberg@flosports.tv>
2022-01-24 13:42:30 -0600

    add query

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -12,30 +12,11 @@ export class TasksService {
     @InjectRepository(TasksRepository)
     private tasksRepository: TasksRepository,
   ) {}
-  // public getAllTasks() {
-  //   return this.tasks;
-  // }
 
-  // public getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
+  public getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
 
-  //   let tasks = this.getAllTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter((task) => {
-  //       if (task.title.includes(search) || task.description.includes(search)) {
-  //         return true;
-  //       }
-  //       return false;
-  //     });
-  //   }
-
-  //   return tasks;
-  // }
   public async getTaskById(id: string): Promise<Task> {
     const found = await this.tasksRepository.findOne(id);
 
